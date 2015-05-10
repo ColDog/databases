9b19083b8cbb8f9fd735033e25464cb0d5eefff2
Colin Walker <cjwalker@sfu.ca>
2015-05-10 00:13:43 -0700

    add registration views

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,7 +1,13 @@
 class RegistrationController < ApplicationController
   def register
+    @courses = Course.all
     @user = current_user
     @register = @user.class_lists.build(register_params)
+  end
+
+  def profile
+    user = current_user
+    @user = user.class_lists
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'registration/register'
 
+  # static pages and information
   root        'static_pages#home'
+
+  # registration/UX controller
+  get         'register'  => 'registration#register'
+  get         'profile'   => 'registration#profile'
+
+  # admin controller
   resources   :courses
+
+  # users devise
   devise_for  :users
 end
