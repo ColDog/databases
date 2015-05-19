767359a4ac9db89a39d1b6e9e481ed49217c5bd7
Colin Walker <cjwalker@sfu.ca>
2015-05-18 20:51:02 -0700

    change sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to user
       else
-        message  = 'Account not activated. '
-        message += 'Check your email for the activation link.'
-        flash[:warning] = message
-        redirect_to root_url
+        flash[:warning] = 'Account currently not activated you will not be able to register for courses, check your email for the link'
+        redirect_to user
       end
     else
       flash.now[:danger] = 'Invalid email/password combination'
