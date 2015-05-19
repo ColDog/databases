63a0f9618a7adffdc456a314aebba15b3245b55b
Colin Walker <cjwalker@sfu.ca>
2015-05-18 23:42:29 -0700

    finish pass reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -25,11 +25,11 @@ class PasswordResetsController < ApplicationController
 
   def update
     if password_blank?
-      flash.now[:danger] = "Password can't be blank"
+      flash.now[:danger] = 'Password cannot be blank'
       render 'edit'
     elsif @user.update_attributes(user_params)
       log_in @user
-      flash[:success] = "Password has been reset."
+      flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
       render 'edit'
@@ -55,7 +55,7 @@ class PasswordResetsController < ApplicationController
 
   # Confirms a valid user.
   def valid_user
-    unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+    unless (@user && @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
     end
   end
@@ -63,7 +63,7 @@ class PasswordResetsController < ApplicationController
   # Checks expiration of reset token.
   def check_expiration
     if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
+      flash[:danger] = 'Password reset has expired.'
       redirect_to new_password_reset_url
     end
   end
